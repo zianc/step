de89550415ccea538ef336841800fad0a4d3c0b7
Zian Chen <zianchen774@gmail.com>
2020-07-06 16:59:18 +0000

    Remove constructor from Constants class

--- portfolio/src/main/java/com/google/sps/data/Constants.java
+++ portfolio/src/main/java/com/google/sps/data/Constants.java
@@ -1,7 +1,6 @@
 package com.google.sps.data;
 
 public class Constants {
-    private Constants() {}
     public static final String COMMENTS_KIND = "Comments";
     public static final String COMMENTS_PROPERTY = "comment";
 }
