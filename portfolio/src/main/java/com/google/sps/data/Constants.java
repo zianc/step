5b89f192d9246ac81d7e851f2aef170ff3178c2e
Zian Chen <zianchen774@gmail.com>
2020-07-07 00:52:28 +0000

    Address Ying's comment by adding limit constant

--- portfolio/src/main/java/com/google/sps/data/Constants.java
+++ portfolio/src/main/java/com/google/sps/data/Constants.java
@@ -3,4 +3,5 @@ package com.google.sps.data;
 public class Constants {
     public static final String COMMENTS_KIND = "Comments";
     public static final String COMMENTS_PROPERTY = "comment";
+    public static final String COMMENTS_LIMIT = "limit";
 }
