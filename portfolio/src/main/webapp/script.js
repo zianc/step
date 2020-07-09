eb235fe258d34ac87fff3517231285330e63fad3
Zian Chen <zianchen774@gmail.com>
2020-07-09 04:46:21 +0000

    Address Razvan's comments by adding hidden div and fixing quotes
    (1) Create a hidden div for login/logout functionalities to reduce excessive
    insertion of elements into DOM using Javascript. (2) Change quotes from
    double to single quote. (3) Edit erroneous and missing comments in
    script.js and Comment.java

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,127 +11,105 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+ 
 /* 
- * If user is not logged in, create a login button that will allow the user to
- * login and add it to the DOM. Otherwise, create the comment input form and 
- * the button to restrict number of comments and add them to the DOM.
+ * Get the login status of the current user. If logged in, display the comment
+ * submission input box and the button to select number of displayed comments.
+ * Display a logout button. If not logged in, hide the comment submission and 
+ * selection of number of displayed comments. Display a login button.
  */
-function getLogin() {
-    fetch("/login")
+function getLoginStatus() {
+    fetch('/login')
     .then(response => response.json())
     .then((result) => {
-        const button_area = document.getElementById("button-area");
-        const msg_form = document.getElementById("msg-form");
-        button_area.innerHTML = '';
+        const button_area = document.getElementById('button-area');
+        const msg_form = document.getElementById('msg-form');
+        const comment_form = document.getElementById('comment-form');
+        const quantity_input = document.getElementById('quantity');
         let button = document.createElement('button');
+        button_area.innerHTML = '';
+ 
         if (result.loggedIn == 1) {
-            /* Allow logged in users to submit comments. */
-            let form = document.createElement('form');
-            form.id = "comment-form";
-            form.action = "/data";
-            form.method = "POST";
-
-            let textarea = document.createElement('textarea');
-            textarea.id = "comment-form-box";
-            textarea.name = "comment";
-            textarea.placeholder = "LEAVE A COMMENT!";
-            form.appendChild(textarea);
-
-            let line_break = document.createElement('br');
-            form.appendChild(line_break);
-
-            let form_input = document.createElement('input');
-            form_input.id = "comment-form-input";
-            form_input.type = "submit";
-            form.appendChild(form_input);
-            form.style.marginTop = "10rem";
-            msg_form.appendChild(form);
-            msg_form.style.width = "75%";
-
-            /* Allow logged in users to change the number of comments. */
-            let input = document.createElement('input');
-            input.addEventListener('change', () => { getComments(input.value); });
-            input.type = "number";
-            input.id = "quantitiy";
-            input.name = "limit";
-            input.min = "0";
-            input.placeholder = "MAX COMMENTS";
-            input.style.padding = "0.5rem";
-            input.style.margin = "1rem";
-            button_area.append(input);
-
+            /* Allow logged in users to submit comments and change number of comments. */
+            comment_form.style.display = 'flex';
+            quantity_input.style.display = 'block';
+ 
+            /* Create button for logging in and out. */
             button.setAttribute('id', 'logout');
-            button.innerHTML = "LOGOUT";
+            button.innerHTML = 'LOGOUT';
             button.addEventListener('click', () => {
                 window.location.href = result.URL;
             });
-            button.style.padding = "0.5rem";
-            button.style.margin = "1rem";
+            button.style.padding = '0.5rem';
+            button.style.margin = '1rem';
             button_area.appendChild(button);
         } else {
+            /* If not logged in, hide comment submission form. */
+            comment_form.style.display = 'none';
+            quantity_input.sytle.display = 'none';
+ 
             /* Create a login button that redirects to login page when clicked. */
             button.setAttribute('id', 'login');
-            button.innerHTML = "LOGIN";
+            button.innerHTML = 'LOGIN';
             button.addEventListener('click', () => {
                 window.location.href = result.URL;
             });
-            button_area.style.padding = "0.5rem";
-            button.style.margin = "1rem";
+            button_area.style.padding = '0.5rem';
+            button.style.margin = '1rem';
             button_area.appendChild(button);
         }
     });
 }
-
+ 
 /* 
  * Fade in items on scroll when their upper bound crosses the bottom of 
  * of the screen.
  */
-$(window).on("load", function() {
+$(window).on('load', function() {
     $(window).scroll(function() {
         var window_bottom = $(this).scrollTop() + $(this).innerHeight();
-
-        $(".fade").each(function() {
+ 
+        $('.fade').each(function() {
             var obj_bottom = $(this).offset().top + ($(this).outerHeight() / 4.0);
             if (obj_bottom < window_bottom) {
-                if ($(this).css("opacity")==0) {
+                if ($(this).css('opacity')==0) {
                     $(this).fadeTo(500, 1);
                 }
             } else {
-                if ($(this).css("opacity") == 1) {
+                if ($(this).css('opacity') == 1) {
                     $(this).fadeTo(500, 0);
                 }
             }
         });
     }).scroll();
 });
-
+ 
 /*
  * Retrieve comments from database. Set limit on the number of comments
  * retrieved through query parameter.
  */
-function addCommentsToDOM(limit) {
+function getComments(limit) {
     if (limit == '')
         return;
-    fetch("/data?limit=".concat(limit))
+    fetch('/data?limit='.concat(limit))
     .then(response => response.json())
     .then((comments) => {
         const container = document.getElementById('msg-container');
         container.innerHTML = '';
         comments.forEach((entry) => {
             const comment = document.createElement('div');
-            comment.style.display = "flex";
-
+            comment.style.display = 'flex';
+ 
             /* Create comment and add text and delete icon. */
             let text = entry.comment;
             const node = document.createElement('p');
             const pnode = document.createTextNode(text);
             node.appendChild(pnode);
             comment.appendChild(node);
-
+ 
             const del = document.createElement('input');
-            del.type = "image";
-            del.src = "images/x_icon.png";
+            del.type = 'image';
+            del.src = 'images/x_icon.png';
             del.addEventListener('click', () => {
                 deleteComment(entry);
                 comment.remove();
@@ -142,51 +120,49 @@ function addCommentsToDOM(limit) {
         })
     })
 }
-
+ 
 /*
  * Load Google Maps.
  */
 function createMap(map_name, latitude, longitude) {
     let myLatLng = {lat: latitude, lng: longitude};
-
+ 
     const map = new google.maps.Map(document.getElementById(map_name), {
         center: myLatLng,
         zoom: 12
     });
-
+ 
     let marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
     });
 }
-
+ 
 /*
- * Append a set of footprints to the DOM, with direction pointing top to bottom,
- * fading in on scroll, to emulate the experience of walking from a location
- * at the top of the screen to the bottom. Takes the ID of the destination 
- * location as an argument.
+ * Creates set of footprints from one div to the next. Each footprint is given
+ * a random horizontal positioning between 3rem to 5rem off center.
  */
-function createFootprints(locationID) {
+function createFootprints(elem_name) {
     const num_footprints = 6;
-    const location = document.getElementById(locationID);
+    const elem = document.getElementById(elem_name);
     for (let i = 0; i < num_footprints; i++) {
-        const footprint = document.createElement("img");
-        footprint.src = "images/footprint.png";
-        const direction = (i % 2 == 0) ? "right" : "left";
-        footprint.classList.add("footprint-" + direction);
+        const footprint = document.createElement('img');
+        footprint.src = 'images/footprint.png';
+        const direction = (i % 2 == 0) ? 'right' : 'left';
+        footprint.classList.add('footprint-' + direction);
         const rand = Math.floor(Math.random * (4) + 3);
-        if (direction.localeCompare("right") == 0) {
-            footprint.style.margin = "1rem 1rem 1rem " + rand + "rem";
+        if (direction.localeCompare('right') == 0) {
+            footprint.style.margin = '1rem 1rem 1rem ' + rand + 'rem';
         } else {
-            footprint.style.margin = "1rem " + rand + "rem 1rem 1rem";
+            footprint.style.margin = '1rem ' + rand + 'rem 1rem 1rem';
         }
-        footprint.classList.add("fade");
-        location.appendChild(footprint);
+        footprint.classList.add('fade');
+        elem.appendChild(footprint);
     }
 }
-
+ 
 /* 
- * fsdf   sdsdfsent using ID as identifier. Does not redirect page after
+ * Delete single comment using ID as identifier. Does not redirect page after
  * deletion.
  */
 function deleteComment(comment) {
